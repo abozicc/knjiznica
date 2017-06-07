f2f07dbc1e72c931a4dbeac73f64c452702eccbc
abozicc <ana.bozic1950@hotmail.com>
2017-06-08 00:42:55 +0200

    Update HelloServer.php

--- HelloServer.php
+++ HelloServer.php
@@ -12,25 +12,21 @@ $dbhandle = mysql_connect($hostname, $username, $password)
 $selected = mysql_select_db("knjiznica",$dbhandle)
   or die("Could not select knjiznica");
 
-$result = mysql_query("SELECT knjiga.naziv_knjige, knjiga.id_knjige, autor.naziv_autora, izdavac.naziv_izdavaca, vrsta_knjiga.vrsta_knjige, izdavac.id_izdavaca
-    FROM knjiga INNER JOIN autor ON knjiga.id_autora = autor.id_autora 
-    INNER JOIN izdavac ON knjiga.id_izdavaca = izdavac.id_izdavaca 
+$result = mysql_query("SELECT knjiga.naziv_knjige, knjiga.id_knjige, izdavac.naziv_izdavaca, vrsta_knjiga.vrsta_knjige
+    FROM knjiga INNER JOIN izdavac ON knjiga.id_izdavaca = izdavac.id_izdavaca 
     INNER JOIN vrsta_knjiga ON knjiga.id_vrstaknjige = vrsta_knjiga.id_vrstaknjige 
-    WHERE izdavac.naziv_izdavaca LIKE '%{$yourName}%'");
+    WHERE izdavac.naziv_izdavaca LIKE '%{$yourName}%' OR vrsta_knjiga.vrsta_knjige LIKE '%{$yourName}%'");
 
   
   while ($row = mysql_fetch_array($result)) {
 	  
     $privremeni = $privremeni
       ."\n Naziv izdavaca: " . $row["naziv_izdavaca"]
-      ."\n ID izdavaca: " . $row["id_izdavaca"]
-      ."\n Naziv knjige: " . $row["naziv_knjige"]
-      ."\n Autor : " . $row["naziv_autora"]
-      ."\n Vrsta knjige: " . $row["vrsta_knjige"]
+      ."\n Vrsta knjige: " . $row["vrsta_knjige"]      
+    ."\n Naziv knjige: " . $row["naziv_knjige"]
+        
       ."\n \n";
 }
-
-
   
   
   //return $jsonn;
